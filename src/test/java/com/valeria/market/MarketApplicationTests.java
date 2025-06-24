22b9dadbe050bd06d6d448669a374590feabc8dc
ValeMP12 <preciadovaleriam@gmail.com>
2025-06-24 13:02:06 -0600

    Inicializando el proyecto

---
+++ src/test/java/com/valeria/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valeria.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
