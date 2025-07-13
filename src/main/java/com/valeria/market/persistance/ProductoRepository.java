6180a3325e55d3347f4a20a3cae9935f19a8682b
ValeMP12 <preciadovaleriam@gmail.com>
2025-07-13 14:04:00 -0600

    Query methods en Sprin Data

--- src/main/java/com/valeria/market/persistance/ProductoRepository.java
+++ src/main/java/com/valeria/market/persistance/ProductoRepository.java
@@ -4,10 +4,17 @@ import com.valeria.market.persistance.crud.ProductoCrudRepository;
 import com.valeria.market.persistance.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
     public List<Producto> getAll() {
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
--- src/main/java/com/valeria/market/persistance/crud/ProductoCrudRepository.java
+++ src/main/java/com/valeria/market/persistance/crud/ProductoCrudRepository.java
@@ -3,5 +3,10 @@ package com.valeria.market.persistance.crud;
 import com.valeria.market.persistance.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
