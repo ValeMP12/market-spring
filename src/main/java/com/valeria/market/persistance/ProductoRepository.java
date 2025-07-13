e805fb0f84a12d0e96d5c4185282e698e43b44e0
ValeMP12 <preciadovaleriam@gmail.com>
2025-07-13 17:31:53 -0600

    Implementacion y beneficios del patron DataMapper

--- src/main/java/com/valeria/market/persistance/ProductoRepository.java
+++ src/main/java/com/valeria/market/persistance/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.valeria.market.persistance;
 
 import com.valeria.market.persistance.crud.ProductoCrudRepository;
 import com.valeria.market.persistance.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
     public List<Producto> getAll() {
@@ -16,5 +18,14 @@ public class ProductoRepository {
     }
     public Optional<List<Producto>> getEscasos(int cantidad) {
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+    public void delete(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
     }
 }
